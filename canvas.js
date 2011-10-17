9b157308f8ecdfdb1e6b12b99f429ec614d10d36
David Mongeau-Petitpas <dmongeau_petitpas@ecoutez.ca>
2011-10-16 22:45:12 -0400

    first commit

---
+++ canvas.js
@@ -0,0 +1,302 @@
+// JavaScript Document
+
+var Canvas = function(el,opts) {
+	
+	this.opts = $.extend({
+		
+		'unit' : 100
+		
+	},opts);
+	
+	
+	this.el = $(el);
+	this.width = $(el).width();
+	this.height = $(el).height();
+	this.widgets = {};
+	this.widgetsCount = 0;
+	this.widgetsOpened = [];
+	this.widgetsClosed = [];
+	
+	this.refreshGrid();
+	
+	console.log(this.grid);
+	
+};
+
+Canvas.prototype = {
+	
+	'refreshGrid' : function() {
+		
+		this.cols = Math.floor(this.width/this.opts.unit);
+		this.rows = Math.floor(this.height/this.opts.unit);
+		var grid = [];
+		for(var i = 0; i < this.rows; i++) {
+			grid[i] = [];
+			for(var ii = 0; ii < this.cols; ii++) {
+				grid[i][ii] = false;
+			}
+		}
+		
+		this.grid = grid;
+		
+	},
+	
+	'createRow' : function(row) {
+		this.grid[row] = [];
+		for(var jjj = 0; jjj < this.grid[0].length; jjj++) {
+			this.grid[row][jjj] = false;
+		}
+		this.rows = this.grid.length;
+	},
+	
+	
+	
+	'addWidget' : function(name, widget) {
+		
+		this.widgets[name] = {
+			'name' : name,
+			'el' : $(widget),
+			'closed' : $(widget).is('.small')
+		};
+		this.widgetsCount++;
+		
+		this.el.append(widget);
+		
+		if(this.widgets[name].closed) {
+			this.widgetsClosed.push(this.widgets[name]);
+		} else {
+			this.widgetsOpened.push(this.widgets[name]);
+		}
+		
+	},
+	
+	'removeWidget' : function(name) {
+		
+		if(this.widgets[name].closed) {
+			var widgetsClosed = [];
+			for(var i = 0; i < this.widgetsClosed; i++) {
+				if(this.widgetsClosed[i].name != name) widgetsClosed.push(this.widgetsClosed[i]);
+			}
+			this.widgetsClosed = widgetsClosed;
+		} else {
+			var widgetsOpened = [];
+			for(var i = 0; i < this.widgetsOpened; i++) {
+				if(this.widgetsOpened[i].name != name) widgetsOpened.push(this.widgetsOpened[i]);
+			}
+			this.widgetsOpened = widgetsOpened;
+		}
+		
+		this.widgets[name] = null;
+		this.widgetsCount--;
+		
+		
+	},
+	
+	'moveWidget' : function($widget,top,left) {
+		if(!$widget.is('.absolute')) {
+			$widget.addClass('absolute');
+		}
+		
+		$widget.animate({
+			'top': top,
+			'left': left
+		},300);
+	},
+	
+	'refresh' : function() {
+		
+		this.width = this.el.width();
+		this.height = this.el.height();
+		
+		this.refreshGrid();
+		
+		for(var z = 0; z < this.widgetsOpened.length; z++) {
+			
+				console.log(this.widgetsOpened[z].name);
+				var $widget = this.widgetsOpened[z].el;
+				
+				var cols = Math.ceil($widget.width()/this.opts.unit);
+				var rows = Math.ceil($widget.height()/this.opts.unit);
+				var pos = this.getFreePosition(rows,cols);
+				
+				this.fillGrid(pos,rows,cols);
+				console.log(this.grid);
+				
+				this.moveWidget($widget, pos.top.px, pos.left.px);
+				
+		}
+		
+		
+		var lastFreeRow = this.getLastFreeRow();
+		console.log('lastFreeRow : ',lastFreeRow);
+		;
+		var closedRows = Math.ceil(this.widgetsClosed.length/this.cols);
+		var i = 0;
+		var col = 0;
+		var lastRow = 0;
+		for(var z = 0; z < this.widgetsClosed.length; z++) {
+			
+			var $widget = this.widgetsClosed[z].el;	
+			
+			var row = lastFreeRow+Math.floor(i/this.cols);
+			if(lastRow != row) {
+				lastRow = row;
+				col = 0;
+			}
+			if(typeof(this.grid[row]) == 'undefined') {
+				this.createRow(row);
+			}
+			this.grid[row][i] = true;
+			
+			this.moveWidget($widget,(row*this.opts.unit)+'px',(col*this.opts.unit)+'px');
+			
+			i++;
+			col++;
+			
+		}
+		
+		console.log('closedWidgets');
+		console.log(this.widgetsClosed);
+		
+		this.adjustHeight();
+		
+		
+	},
+	
+	'adjustHeight' : function() {
+		
+		var lastUsed = this.getLastUsedRow();
+		
+		var minHeight = (lastUsed+1)*this.opts.unit;
+		if(minHeight > this.el.height()) {
+			this.el.height(minHeight+5);
+		} else if(minHeight < (this.el.height()-5)) {
+			this.el.height(minHeight+5);
+		}
+		
+	},
+	
+	'getFreePosition' : function(rows,cols) {
+		
+		for(var i = 0; i < this.rows; i++) {
+			var pos = this.getRowFreePosition(i,rows,cols);
+			if(pos != null) {
+				return pos;
+			}
+		}
+		
+		return {
+			'top' : {
+				'px' : (i*this.opts.unit)+'px',
+				'unit' : i
+			},
+			'left' : {
+				'px' : 0+'px',
+				'unit' : 0
+			}
+		};
+		
+	},
+	
+	
+	'getRowFreePosition' : function(row,rows,cols) {
+		
+		col:
+		for(var ii = 0; ii < this.cols; ii++) {
+			if(this.grid[row][ii] === true) {
+				continue col;
+			}
+			
+			for(var j = 0; j < rows; j++) {
+				if(typeof(this.grid[row+j]) == 'undefined') {
+					this.grid[row+j] = [];
+					for(var jjj = 0; jjj < this.grid[0].length; jjj++) {
+						this.grid[row+j][jjj] = false;
+					}
+					this.rows = this.grid.length;
+				}
+				for(var jj = 0; jj < cols; jj++) {
+					if(typeof(this.grid[row+j][ii+jj]) == 'undefined') {
+						continue col;
+					}
+					if(this.grid[row+j][ii+jj]) {
+						continue col;
+					}
+				}
+			}
+			
+			return {
+				'top' : {
+					'px' : (row*this.opts.unit)+'px',
+					'unit' : row
+				},
+				'left' : {
+					'px' : (ii*this.opts.unit)+'px',
+					'unit' : ii
+				}
+			};
+			
+		}
+		
+		return null;
+		
+	},
+	
+	
+	'fillGrid' : function(pos,rows,cols) {
+		for(var i = 0; i < rows; i++) {
+			for(var ii = 0; ii < cols; ii++) {
+				if(typeof(this.grid[i+pos.top.unit]) == 'undefined') this.grid[i+pos.top.unit] = [];
+				this.grid[i+pos.top.unit][ii+pos.left.unit] = true;
+			}
+		}
+	},
+	
+	'getLastFreeRow' : function() {
+		var lastFreeRow = 0;
+		for(var i = 0; i < this.rows; i++) {
+			for(var ii = 0; ii < this.cols; ii++) {
+				if(this.grid[i][ii]) {
+					lastFreeRow = i+1;
+				}
+			}
+		}
+		
+		return lastFreeRow;
+		
+	},
+	
+	'getLastUsedRow' : function() {
+		var lastUsed = null;
+		for(var i = 0; i < this.grid.length; i++) {
+			for(var ii = 0; ii < this.grid[i].length; ii++) {
+				if(this.grid[i][ii] === true) {
+					lastUsed = i;	
+				}
+			}
+		}
+		if(lastUsed == null) {
+			lastUsed = 0;
+		}
+		return lastUsed;
+		
+	},
+	
+	'getRowFreeColCount' : function(row) {
+		var freeCount = 0;
+		for(var ii = 0; ii < this.grid[0].length; ii++) {
+			if(!this.grid[row][ii]) {
+				if(typeof(this.grid[row][ii]) == 'undefined') {
+					this.grid[row][ii] = false;
+				}
+				freeCount++;
+			}
+		}
+		
+		return freeCount;
+		
+	}
+	
+	
+	
+};
